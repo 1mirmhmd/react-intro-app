191171d12d86665b661de981c5ee931b3ea67b10
mirmhmd <mirmhmdmahmud@gmail.com>
2023-12-23 20:10:14 +0300

    useState konusu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, } from 'react';
 import './App.css';
 
-function App() {
+// JSX - Javascript ile html'in birleştirildiği yapı
+// HTML'de keyword'u değiştirme
+// class -> className
+
+//bu şekilde de export edilebilir
+
+export default function App() { //App() gibi görünümlere component denir.
+  let count = 0;
+  // react hoocks
+  // useState -> değişken tanımlamak için kullanılan hoock
+  // React içerisinde normal bir şekilde tanımlanan değişkenler takip edilmez, UI update yapılmaz. Bu yüzden useState hoock'u kullanılır
+  // getTotal, setTotal=> initial value
+
+  const [ total, setTotal ] = useState(0);
+  // ES7 Extension ile yukarıdaki syntax useStateSnippet şeklinde oluşturulabilir 
+  const Increase = () => {
+    setTotal(total + 1);
+    // count++;
+    // console.log(count);
+  }
+  const Decrease = () => {
+    setTotal(total - 1);
+    // count--;
+    // console.log(count);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    // İki etiketi peşpeşe kullanamayız, Etiketler tek element altında olmalı. 
+    // Bu durumu bypass etmek için React.Fragment veya boş etiket <> </> açılıp kapatılır.
+    <React.Fragment>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {total}
+          {/* html'de JS kodu yazmak için {} süslü oarantez kullanılır */}
+          {/* {count}"<br></br>"
+          {count = 5} */}
+          <br></br>
+          Merhaba
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div>
+        <p >
+          <button onClick={Increase}>Increase</button>  <br></br>
+          <button onClick={Decrease}>Decrease</button>
+
+        </p>
+      </div>
+    </React.Fragment>
   );
 }
+export const var1 = 1;
 
-export default App;
+// JS'te bir nesnenin import edilebilmesi için o nesnenin export edilmesi gerekiyor
+// default olarak export edilmeyen şeyi yukarıdaki gibi doğrudan export etmeliyiz.
+// export default App;
+
